cf875ad8e67ca64f7b1a22c01e4194501b5f190d
Giovanni Guerreiro <guerreiro.giovanni@gmail.com>
2022-04-06 08:11:09 +0200

    Maven and JUNIT tests passed for Withdraw cards from a deck.

--- cucumber-test/src/main/java/model/Player.java
+++ cucumber-test/src/main/java/model/Player.java
@@ -5,7 +5,16 @@ import java.util.ArrayList;
 public class Player {
 	
 	private boolean turnStatus = false;
+	private int playerID;
+	ConsoleMessage response = new ConsoleMessage();
 	ArrayList<Card> hand = new ArrayList<Card>(9);
+	ArrayList<Card> turnHand = new ArrayList<Card>(5);
+
+	
+	public void setID(Integer playerID) {
+		
+		this.playerID = playerID;
+	}
 
 	public void setTurnStatus(boolean turnStatus) {
 		this.turnStatus = turnStatus;
@@ -14,7 +23,6 @@ public class Player {
 	
 	public ConsoleMessage setHand(ArrayList<Card> hand) {
 		
-		ConsoleMessage response = new ConsoleMessage();
 		
 		this.hand = hand;
 		response.setMessage("9 cards were dealt!");
@@ -22,5 +30,16 @@ public class Player {
 		return response;
 		
 	}
+
+	public ConsoleMessage setTurnCards(ArrayList<Card> turnHand) {
+		
+		this.turnHand = turnHand;
+		response.setMessage("5 cards were selected for the round!");
+		return response;
+
+		
+	}
+
+
 
 }
--- cucumber-test/src/test/resources/FeatureFilesGiovanni/Player.feature
+++ cucumber-test/src/test/resources/FeatureFilesGiovanni/Player.feature
@@ -1,9 +1,19 @@
 @tag
 Feature: Player getting cards
+	
+	  Background:
+    Given A player with ID of 203492 
+    And player turn status true
 
   @tag1
   Scenario: Player get 9 programming cards cards
-    Given A player with is turn status true
     When GameMaster starts a new round
     Then player receives 9 programming cards 
     And console should display a sucessful message
+    
+    
+  Scenario: Player selects 5 card for the his turn
+    Given player receives 9 programming cards 
+    When Player selects 5 cards from his hand for hist turn
+    Then console should display a sucessul message
+    
