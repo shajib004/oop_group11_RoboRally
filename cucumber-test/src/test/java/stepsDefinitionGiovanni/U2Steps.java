46f84ef7a3dc3f896774c5d62a16c34ab6ef6994
Giovanni Guerreiro <guerreiro.giovanni@gmail.com>
2022-04-15 08:58:07 +0200

    new changes

--- cucumber-test/src/test/java/stepsDefinitionGiovanni/U2Steps.java
+++ cucumber-test/src/test/java/stepsDefinitionGiovanni/U2Steps.java
@@ -16,22 +16,25 @@ public class U2Steps {
 		Board board = new Board();
 	
 	@Given("robot on the board in x {int} and y {int}")
-	public void robot_on_the_board_in_x_and_y(Integer int1, Integer int2) {
-//		board.placeRobot(new Position(int1, int2), robot);
+	public void robot_on_the_board_in_x_and_y(Integer x, Integer y) {
+		board.placeRobot(new Position(x, y), robot);
 	}
 
 	@When("move")
 	public void move() {
-//	    board.moveRobot(robot);
+	    board.moveRobot(robot, );
 	}
 
-	@Then("robot should be on coordinate x_coordinate {int} and y_coordinate {int}")
-	public void robot_should_be_on_coordinate_x_coordinate_and_y_coordinate(Integer int1, Integer int2) {
-//	    assertEquals(10, 10);
+	@Then("robot should be on coordinate x {int} and y {int}")
+	public void robot_should_be_on_coordinate_x_and_y(Integer int1, Integer int2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
 	}
 
-	@Then("pit status is true")
-	public void pit_status_is_true() {
+	@Then("tile on the new coordinate ia a Pit")
+	public void tile_on_the_new_coordinate_ia_a_pit() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
 	}
 
 	@Then("player life status is reduced by {int}")
--- cucumber-test/src/test/resources/FeatureFilesGiovanni/U2RobotInteraction/U2RobotInteraction.feature
+++ cucumber-test/src/test/resources/FeatureFilesGiovanni/U2RobotInteraction/U2RobotInteraction.feature
@@ -1,13 +1,12 @@
 Feature: Interacting with Obstacles
 
-	#Test
 	Background:
 		Given robot on the board in x 1 and y 1
 
-  Scenario: Robot falling into a Pit and Loosing his lifes
+  Scenario: Robot falling into a Pit -> Loosing his lifes and Returning to CheckPoint
     When move
-    Then robot should be on coordinate x_coordinate 2 and y_coordinate 1
-    And pit status is true
+    Then robot should be on coordinate x 2 and y 1
+    And tile on the new coordinate ia a Pit
     And player life status is reduced by 1
     And robot is moved to checkpoint
     
